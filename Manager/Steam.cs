a391a361161a7085fb013730a7a538e257f3b83d
elirasza <thibaud.laurent@elira.fr>
2025-01-25 23:05:47 +0100

    fixed windows not being able to retrieve game path

--- Manager/Steam.cs
+++ Manager/Steam.cs
@@ -1,43 +1,54 @@
 using System.Runtime.InteropServices;
 using Gameloop.Vdf;
-using Gameloop.Vdf.Linq;
 using Microsoft.Win32;
 
 namespace LethalCompanyModManager
 {
     public static class Steam
     {
-        public const string KEY_STEAM_STEAM = "Steam";
+        public const string KEY_STEAM = "Steam";
         public const string KEY_STEAM_STEAMAPPS = "steamapps";
         public const string KEY_STEAM_STEAMAPPS_COMMON = "common";
         public const string KEY_STEAM_LIBRARYFOLDERS = "libraryfolders.vdf";
         public const string KEY_LIBRARYFOLDERS_PATH = "path";
         public const string LOG_ERROR_PATH_STEAM_NOT_FOUND = "Could not find path {0}.";
-        public const string LOG_ERROR_PATH_GAME_NOT_FOUND = "Could not find game path {0}.";
-        public const string REGISTRY_STEAM = "HKEY_LOCAL_MACHINE\\Software\\Valve\\Steam";
-        public const string REGISTRY_STEAM_KEY = "InstallPath";
+        public const string LOG_ERROR_PATH_GAME_NOT_FOUND = "Could not find game path.";
+        public const string REGISTRY_STEAM = "HKEY_CURRENT_USER\\Software\\Valve\\Steam";
+        public const string REGISTRY_STEAM_KEY = "SteamPath";
 
         public static string GetSteamLibraryForGame(string game)
         {
-            var pathSteam = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
-                ? Path.Join(Registry.GetValue(REGISTRY_STEAM, REGISTRY_STEAM_KEY, "")?.ToString(), KEY_STEAM_STEAMAPPS, KEY_STEAM_LIBRARYFOLDERS)
-                : Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), KEY_STEAM_STEAM, KEY_STEAM_STEAMAPPS, KEY_STEAM_LIBRARYFOLDERS);
+            var isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+            var isLinux = !isWindows;
+
+            var pathSteam = isWindows
+                ? Path.Join(Registry.GetValue(REGISTRY_STEAM, REGISTRY_STEAM_KEY, "")?.ToString())
+                : Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), KEY_STEAM);
 
             if (!Path.Exists(pathSteam))
             {
                 throw new FileNotFoundException(string.Format(LOG_ERROR_PATH_STEAM_NOT_FOUND, pathSteam));
             }
 
-            var root = VdfConvert.Deserialize(File.ReadAllText(pathSteam));
+            var pathSteamLibraries = Path.Join(pathSteam, KEY_STEAM_STEAMAPPS, KEY_STEAM_LIBRARYFOLDERS);
 
-            var pathGame = root
+            var vdfSettings = new VdfSerializerSettings()
+            {
+                UsesEscapeSequences = false,
+                IsWindows = isWindows,
+                IsLinux = isLinux,
+            };
+
+            var vdf = VdfConvert.Deserialize(File.ReadAllText(pathSteamLibraries), vdfSettings);
+
+            var pathGame = vdf
                 .Value.Select(key => VdfConvert.Deserialize(key.ToString()).Value[KEY_LIBRARYFOLDERS_PATH]?.ToString() ?? "")
                 .Select(path => Path.Join(path, KEY_STEAM_STEAMAPPS, KEY_STEAM_STEAMAPPS_COMMON, game))
-                .First(Path.Exists);
+                .FirstOrDefault(Path.Exists, "");
 
-            if (!Path.Exists(pathGame))
+            if (pathGame.Length == 0)
             {
-                throw new FileNotFoundException(string.Format(LOG_ERROR_PATH_GAME_NOT_FOUND, pathGame));
+                throw new FileNotFoundException(LOG_ERROR_PATH_GAME_NOT_FOUND);
             }
 
             return pathGame;
